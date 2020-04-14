a1851743cec339f76aef39fbcc198f0661930589
Niall O'Callaghan <niall@elev.io>
2020-04-14 23:37:18 +1000

    Added xstate machine

---
+++ src/state/machine.ts
@@ -0,0 +1,21 @@
+import { Machine } from 'xstate';
+import { TrafficState } from './types';
+import { TRANSITION } from './actions';
+
+export const crossRoads = Machine({
+  id: 'crossRoads',
+  initial: TrafficState.stopped,
+  states: {
+    [TrafficState.stopped]: {
+      on: { [TRANSITION]: TrafficState.flowing },
+    },
+    [TrafficState.flowing]: {
+      on: { [TRANSITION]: TrafficState.stopping },
+    },
+    [TrafficState.stopping]: {
+      on: {
+        [TRANSITION]: TrafficState.stopped,
+      },
+    },
+  },
+});
