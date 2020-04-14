9666eeb218078a0f49a68a7272781eb038947fe5
Niall O'Callaghan <niall@elev.io>
2020-04-14 23:56:31 +1000

    Added gh-pages. Updated page title. Added link to source code.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+/** @jsx jsx */
+import { jsx, css } from '@emotion/core';
 import './App.css';
 import { TrafficLight } from './components/TrafficLight/TrafficLight';
 import { LightType } from './state/types';
@@ -10,6 +11,18 @@ function App() {
         <TrafficLight type={LightType.traffic} />
         <p>
           <code>XState Traffic Lights</code>
+        </p>
+        <p>
+          <a
+            css={css`
+              color: white;
+            `}
+            href="https://github.com/noccer/xstate-traffic-lights"
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <code>Source code</code>
+          </a>
         </p>
       </header>
     </div>
