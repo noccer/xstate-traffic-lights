b8662a77455a3676905d4ee1a138d30b7ed78de0
Niall O'Callaghan <niall@elev.io>
2020-04-14 23:51:44 +1000

    Updates

---
+++ precache-manifest.ac7933475da7443ee5a67fd7bb680893.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e706995fcc4cdf62c655f6fe7395c17d",
+    "url": "/xstate-traffic-lights/index.html"
+  },
+  {
+    "revision": "500570a98a0db6820856",
+    "url": "/xstate-traffic-lights/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8b79c4d7d78fb6cf41ba",
+    "url": "/xstate-traffic-lights/static/js/2.5d8558a8.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/xstate-traffic-lights/static/js/2.5d8558a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "500570a98a0db6820856",
+    "url": "/xstate-traffic-lights/static/js/main.0841e77d.chunk.js"
+  },
+  {
+    "revision": "0b1b99f50e21b44c0bad",
+    "url": "/xstate-traffic-lights/static/js/runtime-main.f3fd950c.js"
+  }
+]);
